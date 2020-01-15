8da399a5f7c97cac876368bc32f4fc96b162c3da
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-14 21:50:21 -0500

    Creates camera in scene file.

--- js/scene.js
+++ js/scene.js
@@ -1,3 +1,4 @@
 import { TetrahedronGeometry } from "./three";
 
-let scene = new TetrahedronGeometry.Scene();
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );

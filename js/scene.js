280ace2bde5fd68585803501c8413ac2d86d3d12
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-14 21:51:26 -0500

    Creates renderer.

--- js/scene.js
+++ js/scene.js
@@ -2,3 +2,5 @@ import { TetrahedronGeometry } from "./three";
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+let renderer = new THREE.WebGLRenderer();

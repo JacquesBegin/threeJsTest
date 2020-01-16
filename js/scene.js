86475b9f97759fdee1ec50a37795d7b133be926f
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-15 22:30:07 -0500

    Creates the 3D cube object.

--- js/scene.js
+++ js/scene.js
@@ -9,3 +9,4 @@ document.body.appendChild(renderer.domElement);
 
 let geometry = new THREE.BoxGeometry(1, 1, 1);
 let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let cube = new THREE.Mesh(geometry, material);

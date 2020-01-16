1a2a6719cea305959e1bba36fdc8d7904e5c8024
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-15 23:04:46 -0500

    Updates mesh property color and wireframe to true.

--- js/scene.js
+++ js/scene.js
@@ -7,7 +7,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 let geometry = new THREE.BoxGeometry(1, 1, 1);
-let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let material = new THREE.MeshBasicMaterial({ color: 0x00aaff, wireframe: true });
 let cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);

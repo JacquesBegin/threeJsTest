ca0c3eeda7f676bee18d92df082f385c1ccb6034
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-15 22:30:30 -0500

    Adds the cube to the scene.

--- js/scene.js
+++ js/scene.js
@@ -10,3 +10,5 @@ document.body.appendChild(renderer.domElement);
 let geometry = new THREE.BoxGeometry(1, 1, 1);
 let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 let cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);

23f41e77c8b3c4f63af4125bca710e18136d63e0
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-18 22:19:56 -0500

    Adds query selector for canvas.

--- js/scene.js
+++ js/scene.js
@@ -1,6 +1,8 @@
 let THREE = require("three");
 
 
+const canvas = document.querySelector("#c");
+
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 

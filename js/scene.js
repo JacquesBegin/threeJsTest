16fc471e757908813671ba9d6f3ee40d9e641cee
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-18 22:32:26 -0500

    Adds canvas to renderer.

--- js/scene.js
+++ js/scene.js
@@ -6,7 +6,7 @@ const canvas = document.querySelector("#c");
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-let renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer({canvas: canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 

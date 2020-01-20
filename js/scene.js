a5903833a3bb00701ffac2595d80ccf7e5429812
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-19 23:16:16 -0500

    Organizes code.

--- js/scene.js
+++ js/scene.js
@@ -5,6 +5,7 @@ const canvas = document.querySelector("#c");
 
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 3;
 
 let renderer = new THREE.WebGLRenderer({canvas: canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -13,16 +14,15 @@ document.body.appendChild(renderer.domElement);
 let geometry = new THREE.BoxGeometry(1, 1, 1);
 let material = new THREE.MeshBasicMaterial({ color: 0x00aaff, wireframe: true });
 let cube = new THREE.Mesh(geometry, material);
-
 scene.add(cube);
 
-let geometry1 = new THREE.BoxGeometry(1, 1, 1);
-let material1 = new THREE.MeshBasicMaterial({ color: 0x00aaff, wireframe: true });
-let cube1 = new THREE.Mesh(geometry1, material1);
 
-scene.add(cube1);
+// let geometry1 = new THREE.BoxGeometry(1, 1, 1);
+// let material1 = new THREE.MeshBasicMaterial({ color: 0x00aaff, wireframe: true });
+// let cube1 = new THREE.Mesh(geometry1, material1);
+// scene.add(cube1);
 
-camera.position.z = 3;
+
 
 function animate() {
   requestAnimationFrame(animate);

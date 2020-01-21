1824e03a27c4cfe6e49b7464dc06352b1d8f1947
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-20 22:22:49 -0500

    Renames cubes.

--- js/bundle.js
+++ js/bundle.js
@@ -6,7 +6,7 @@ function create3DBox(geo, color, x, y) {
   const geometry = new THREE.BoxGeometry(geo[0], geo[1], geo[2]);
   const material = new THREE.MeshPhongMaterial({color});
   const cube = new THREE.Mesh(geometry, material);
-  // scene.add(cube);
+  scene.add(cube);
   cube.position.x = x;
   if (y) cube.position.y = y;
   return cube;
@@ -33,17 +33,20 @@ scene.add(light);
 
 let geometry = new THREE.BoxGeometry(1, 1, 1);
 let material = new THREE.MeshBasicMaterial({ color: 0x00aaff, wireframe: true });
-let cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+let cube1 = new THREE.Mesh(geometry, material);
+scene.add(cube1);
 
 let geometry1 = new THREE.BoxGeometry(1, 1, 1);
 let material1 = new THREE.MeshPhongMaterial({ color: 0x00aaff });
-let cube1 = new THREE.Mesh(geometry1, material1);
-scene.add(cube1);
+let cube2 = new THREE.Mesh(geometry1, material1);
+scene.add(cube2);
 
-scene.add(create3DBox([1, 1, 1], "#ff00aa", 1.4, 1));
-scene.add(create3DBox([0.4, 0.4, 0.4], "#aaff00", -1.4));
-scene.add(create3DBox([0.7, 0.7, 0.7], "#333333", -0.8, -1));
+const cubes = [
+  create3DBox([1, 1, 1], "#ff00aa", 1.4, 1),
+  create3DBox([0.4, 0.4, 0.4], "#aaff00", -1.4),
+  create3DBox([0.7, 0.7, 0.7], "#333333", -0.8, -1)
+]
+
 
 
 
@@ -53,13 +56,17 @@ scene.add(create3DBox([0.7, 0.7, 0.7], "#333333", -0.8, -1));
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  cube.rotation.z += 0.01;
-  cube.position.y = 1.5;
-  cube1.rotation.x -= 0.01;
-  cube1.rotation.y -= 0.01;
-  cube1.rotation.z -= 0.01;
+  cube1.rotation.x += 0.01;
+  cube1.rotation.y += 0.01;
+  cube1.rotation.z += 0.01;
+  cube1.position.y = 1.5;
+  cube2.rotation.x -= 0.01;
+  cube2.rotation.y -= 0.01;
+  cube2.rotation.z -= 0.01;
+
+  cubes.forEach((cube) => {
+
+  });
 
   renderer.render(scene, camera);
 }

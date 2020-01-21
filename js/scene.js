f89ecc97b86612299a5edcb1ea94d2c675c05f87
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-20 21:25:44 -0500

    Adds original cube back into scene.

--- js/scene.js
+++ js/scene.js
@@ -32,7 +32,7 @@ scene.add(light);
 let geometry = new THREE.BoxGeometry(1, 1, 1);
 let material = new THREE.MeshBasicMaterial({ color: 0x00aaff, wireframe: true });
 let cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
+scene.add(cube);
 
 let geometry1 = new THREE.BoxGeometry(1, 1, 1);
 let material1 = new THREE.MeshPhongMaterial({ color: 0x00aaff });
@@ -52,6 +52,7 @@ function animate() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   cube.rotation.z += 0.01;
+  cube.position.y = 1.5;
   cube1.rotation.x -= 0.01;
   cube1.rotation.y -= 0.01;
   cube1.rotation.z -= 0.01;
